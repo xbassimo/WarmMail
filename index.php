cfb97046fa5301889742fc8d4f5b398f85538a9b
root <root@centos.linux>
2015-04-06 04:49:27 +0800

    index.php中添加xhprof分析器

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
+xhprof_enable(XHPROF_FLAGS_CPU+XHPROF_FLAGS_MEMORY);//加上这个参数可以使得xhprof显示cpu和内存相关的数据。
+
 // 应用入口文件
 
 // 检测PHP环境
@@ -22,5 +24,16 @@ define('APP_PATH','./App/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
-
 // 亲^_^ 后面不需要任何代码了 就是如此简单
+
+$data = xhprof_disable();
+//得到统计数据之后，以下的工作就是为页面显示做准备。
+$xhprof_root = "/var/www/html";//这里填写的就是你的xhprof的路径
+
+include_once $xhprof_root."/xhprof_lib/utils/xhprof_lib.php";
+include_once $xhprof_root."/xhprof_lib/utils/xhprof_runs.php";
+
+$xhprof_runs = new XHprofRuns_Default();
+$xhp_trace = 'tp_xhp_trace';
+$run_id = $xhprof_runs->save_run($data, $xhp_trace);//第二个参数在接下来的地方作为命名空间一样的概念来使用
+echo "<a href='/xhprof_html/index.php?run=".$run_id."&source=".$xhp_trace."'><font color='red'>xhprof--[性能分析器]</font></a>";
